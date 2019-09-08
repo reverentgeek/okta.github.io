98a4ed1115c48a15952fb53c9ffbba5be171b4ff
Randall Degges <r@rdegges.com>
2019-09-07 22:35:07 -0700

    Removing send_promotion_message helper. No longer used.

--- scripts/helpers.sh
+++ scripts/helpers.sh
@@ -46,13 +46,6 @@ function fold() {
     echo -en "travis_fold:end:${name}\\r"
 }
 
-function send_promotion_message() {
-    curl -H "Authorization: Bearer ${TESTSERVICE_SLAVE_JWT}" \
-      -H "Content-Type: application/json" \
-      -X POST -d "[{\"artifactId\":\"$1\",\"repository\":\"npm-okta\",\"artifact\":\"$2\",\"version\":\"$3\",\"promotionType\":\"ARTIFACT\"}]" \
-      -k "${APERTURE_BASE_URL}/v1/artifact-promotion/createPromotionEvent"
-}
-
 function removeHTMLExtensions() {
     # Removing all generated .html files (excludes the main 'index.html' in the dir) and
     # create 302 redirects to extensionless pages
